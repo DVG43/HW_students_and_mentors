e65c74eca621c0c23d7cd954b082a9e15925f521
Galuta Dmitrii <dvgaluta@gmail.com>
2021-12-12 22:38:55 +0300

    Commit first task

--- main.py
+++ main.py
@@ -9,12 +9,22 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
